f744dd17db7169979cae484df8864dbd5118d5cd
morthur007 <matheusruben07@gmail.com>
2023-10-23 02:24:06 -0300

    teste

--- pages/api/[id].js
+++ pages/api/[id].js
@@ -1,13 +1,27 @@
-async function pesquisabylinha(request, response){
-    try{
-        const linhaApi = await fetch(`https://www.sistemas.dftrans.df.gov.br/linha/numero/${request.query.id}`)
-    }catch{
-        console.log(error)
-    }
+import fetch from 'node-fetch';
 
-    response.json({
-        linha: "oi"
-    })
+async function pesquisabylinha(request, response) {
+    try {
+        console.log('teste1');
+        const linhaApi = await fetch(`https://www.sistemas.dftrans.df.gov.br/linha/numero/${request.query.id}`);
+
+        if (!linhaApi.ok) {
+            throw new Error(`Failed to fetch data. Status: ${linhaApi.status}`);
+        }
+
+        const linhaApiJson = await linhaApi.json();
+        console.log('teste2');
+        console.log(linhaApiJson);
+
+        response.json({
+            linha: linhaApiJson[0].numero,
+            tarifa: linhaApiJson[0].faixaTarifaria.tarifa,
+            rota: linhaApiJson[0].descricao,
+        });
+    } catch (error) {
+        console.error('Error:', error.message);
+        response.status(500).json({ error: 'Internal Server Error' });
+    }
 }
 
 export default pesquisabylinha;
