f2e6ef5ad3540b3f7e3f5cd975ec444226ea9e29
morthur007 <matheusruben07@gmail.com>
2023-10-23 02:05:05 -0300

    erro 1

--- pages/api/[id].js
+++ pages/api/[id].js
@@ -1,12 +1,15 @@
 async function pesquisabylinha(request, response){
     const linhaApi = await fetch(`https://www.sistemas.dftrans.df.gov.br/linha/numero/${request.query.id}`)
     const linhaApiJson = await linhaApi.json()
+    console.log('teste')
+    console.log(linhaApiJson)
     
     
 
     response.json({
         linha: linhaApiJson[0].numero,
         tarifa: linhaApiJson[0].faixaTarifaria.tarifa,
+        rota: linhaApiJson[0].descricao,
     })
 }
 
