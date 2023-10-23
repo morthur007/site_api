d33b22fc0773ec5676bcc8f9faf6e5ca59be8166
morthur007 <matheusruben07@gmail.com>
2023-10-23 03:23:35 -0300

    teste 2

--- pages/api/[id].js
+++ pages/api/[id].js
@@ -1,27 +1,19 @@
-import fetch from 'node-fetch';
-
-async function pesquisabylinha(request, response) {
-    try {
-        console.log('teste1');
-        const linhaApi = await fetch(`https://www.sistemas.dftrans.df.gov.br/linha/numero/${request.query.id}`);
-
-        if (!linhaApi.ok) {
-            throw new Error(`Failed to fetch data. Status: ${linhaApi.status}`);
-        }
-
-        const linhaApiJson = await linhaApi.json();
-        console.log('teste2');
-        console.log(linhaApiJson);
-
-        response.json({
-            linha: linhaApiJson[0].numero,
-            tarifa: linhaApiJson[0].faixaTarifaria.tarifa,
-            rota: linhaApiJson[0].descricao,
-        });
-    } catch (error) {
-        console.error('Error:', error.message);
-        response.status(500).json({ error: 'Internal Server Error' });
+async function pesquisabylinha(request, response){
+    const linhaApi = await fetch(`https://www.sistemas.dftrans.df.gov.br/linha/numero/${request.query.id}`, { mode: "no-cors"})
+    const linhaApiJson = await linhaApi.json()
+    
+    let sentidos = []
+    for(var i = 0; i < Object.keys(linhaApiJson).length; i++){
+        sentidos.push(linhaApiJson[i].sentido)
     }
+
+    response.json({
+        linha: linhaApiJson[0].numero,
+        tarifa: linhaApiJson[0].faixaTarifaria.tarifa,
+        sentidos: sentidos,
+        rota: linhaApiJson[0].descricao,
+        numSentados: linhaApiJson[0].tiposOnibus[0].numSentados
+    })
 }
 
 export default pesquisabylinha;
