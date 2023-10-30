64a5dd09ca6db1cf417bf04dc6f9a36be0c760cc
morthur007 <matheusruben07@gmail.com>
2023-10-30 03:18:35 -0300

    arrumando sentidos em [id]

--- pages/api/numero/[id].js
+++ pages/api/numero/[id].js
@@ -3,16 +3,19 @@ async function pesquisabylinha(request, response){
 		const linhaApi = await fetch(`https://www.sistemas.dftrans.df.gov.br/linha/numero/${request.query.id}`)
 		const linhaApiJson = await linhaApi.json()
 		
-		let sentidos = {}
-		for(var i = 0; i < Object.keys(linhaApiJson).length; i++){
-			sentidos[`sentido${i}`] = linhaApiJson[0].sendido
-		}
+		var sentidos = linhaApiJson[0].sentido
+		if (sentidos == "IDA" || sentidos == "VOLTA")
+			sentidos = "Ida e volta"
+		else if(sentidos != "" || sentidos != null && sentidos == "CIRCULAR")
+			sentidos = "Circular"
+		else
+			sentidos = "Desconhecido"
 
 		response.json({
 			condition:"true",
 			linha: linhaApiJson[0].numero,
 			tarifa: linhaApiJson[0].faixaTarifaria.tarifa,
-			sentidos,
+			sentido: sentidos,
 			rota: linhaApiJson[0].descricao,
 			numSentados: linhaApiJson[0].tiposOnibus[0].numSentados
 		})
