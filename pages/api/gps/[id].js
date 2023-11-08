afa45a4253deef351aae1e21b313d8ac31298925
morthur007 <matheusruben07@gmail.com>
2023-11-08 00:08:27 -0300

    real time bus

---
+++ pages/api/gps/[id].js
@@ -0,0 +1,31 @@
+var apiUrl = 'https://www.sistemas.dftrans.df.gov.br/gps/linha/'
+
+async function main(req, res){
+    let numero = req.query.id;
+
+    const resultNoJson = await fetch(`${apiUrl+numero}/geo/recent`);
+    const result = await resultNoJson.json();
+    let linhas = result.features;
+
+    let coordenadas = {onibus:[]};
+    var j=0;
+    for(var i = 0; i < Object.keys(linhas).length; i++){
+        const formt = {latitude: linhas[0].geometry.coordinates[1],longitude: linhas[0].geometry.coordinates[0]}
+        coordenadas.onibus[i] = formt;
+    }
+    var linha = linhas[0].properties.linha
+
+    const retorno = {
+        linha: linha,
+        onibus: coordenadas.onibus
+    }
+
+    
+
+    res.json(retorno)
+    
+
+}
+
+export default main;
+
