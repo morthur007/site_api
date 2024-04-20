f3b5289473de9639e567e0be88d8900c8795db56
morthur007 <matheusruben07@gmail.com>
2024-04-19 21:45:28 -0300

    ha

--- pages/api/travel/[origem]/[destino].js
+++ pages/api/travel/[origem]/[destino].js
@@ -135,6 +135,7 @@ async function getDistance(origem, destino) {
 
         if (response.data.routes.length > 0) {
             const distance = response.data.routes[0].distance;
+            console.log(response)
             return distance
         } else {
             console.log('Não foi possível encontrar uma rota entre as duas coordenadas.');
