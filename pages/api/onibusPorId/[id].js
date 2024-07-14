16e09d5e4bb2b041be2fefb2a1daea3dbde2b6c7
morthur007 <matheusruben07@gmail.com>
2024-07-14 01:59:21 -0300

    adicionando onibusPorId

---
+++ pages/api/onibusPorId/[id].js
@@ -0,0 +1,34 @@
+const fetch = require('node-fetch');
+var apiUrl = "https://geoserver.semob.df.gov.br/geoserver/semob/wfs?service=WFS&request=GetFeature&typeName=semob:Ultima%20Posicao%20Transmitida&outputFormat=json"
+
+async function main(req, res){
+  try{
+    
+    let numero = req.query.id;
+
+    const resultNoJson = await fetch(apiUrl);
+    const result = await resultNoJson.json();
+    const onibusNoPlural = result.features
+
+    onibusNoPlural.forEach((onibusAtual) => {
+      const properties = onibusAtual.properties
+      const id = properties.imei;
+      if(id === numero){
+        res.json({
+          result: "true",
+          linha: linhas[0].properties.linha,
+          id: properties.numerolinha,
+          latitude: properties.latitude,
+          longitude: properties.longitude
+
+        })
+      }
+    });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Erro Interno do Servidor');
+  }    
+}
+
+export default main;
